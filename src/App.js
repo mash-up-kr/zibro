2a7e47f254e2fa0e1190e8492616d6c9a6690e92
chayeoi <chayeoikeem@gmail.com>
2019-06-28 00:26:24 +0900

    chore: test deploy previews

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
 
-const App = () => <div>App</div>;
+const App = () => <div>Deploy Preview!</div>;
 
 export default hot(App);
