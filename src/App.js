f479fc9bd9923dd8d26effa9896491411f0216aa
chayeoi <chayeoikeem@gmail.com>
2019-06-29 10:49:08 +0900

    chore: split chunk depending on route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import { hot } from 'react-hot-loader/root';
-import React from 'react';
+import React, { Suspense } from 'react';
+import {
+  BrowserRouter as Router, Route, Switch,
+} from 'react-router-dom';
+import { Home } from './pages';
 
-const App = () => <div>App</div>;
+
+const App = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Suspense>
+  </Router>
+);
 
 export default hot(App);
