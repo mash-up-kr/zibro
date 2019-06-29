20fa4adcda4d7171192c231047fb42b46436b206
chayeoi <chayeoikeem@gmail.com>
2019-06-29 13:17:48 +0900

    chore(eslint): set babel-eslint parser to eslint config
    support dynamic import syntax

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,27 +2,22 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    jest: true,
   },
   extends: 'airbnb',
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  parser: 'babel-eslint',
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
+    ecmaFeatures: { jsx: true },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react'],
   rules: {
-    'no-underscore-dangle': [
-      'error',
-      { allow: ['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] },
-    ],
+    'no-underscore-dangle': ['error', { allow: ['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] }],
     'no-useless-escape': ['off', 'always'],
     'import/no-extraneous-dependencies': ['error', { 'devDependencies': ['**/*.stories.js', '**/*.test.js'] }],
     'import/prefer-default-export': 'off',
