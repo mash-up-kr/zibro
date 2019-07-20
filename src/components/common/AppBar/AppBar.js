a6c3bbd91d94027819acb9eec445efd23a5ad8cc
aereeeee <dofl5576@gmail.com>
2019-07-21 04:28:43 +0900

    chore: add mock data of detail routes

--- src/components/common/AppBar/AppBar.js
+++ src/components/common/AppBar/AppBar.js
@@ -6,12 +6,6 @@ import { AppBar as MuiAppBar, Toolbar, Typography } from '@material-ui/core';
 import BackButton from '../BackButton';
 
 const S = {
-  Wrapper: styled.div`
-    display: flex;
-    flex-direction: column;
-    flex-grow: 1;
-    height: 100%;
-  `,
   AppBar: styled(MuiAppBar)`
     box-shadow:none;
     background-color:#3e50b4;
--- src/components/reservation-details/RouteField/RouteField.js
+++ src/components/reservation-details/RouteField/RouteField.js
@@ -25,9 +25,40 @@ const S = {
   `,
   DetailRoute: styled(DetailRoute)`
     padding-left: 58px;
+    .cost{
+      display:none;
+    }
   `,
 };
-
+// mock 데이터
+const steps = [
+  {
+    step: 0,
+    stepDuration: '7분',
+    instructions: '남서울농협남현동지점까지 도보',
+    travelMode: 'WALKING',
+  },
+  {
+    step: 1,
+    instructions: '버스 상계주공7단지행',
+    transitDetail: {
+      arrivalStopName: '강남역.역삼세무서',
+      departureStopName: '남서울농협남현동지점',
+      headSign: '상계주공7단지',
+      transitColor: '#374ff2',
+      transitNumber: 'N61',
+      transitType: 'BUS',
+      numStops: 15,
+    },
+    travelMode: 'TRANSIT',
+  },
+  {
+    step: 2,
+    stepDuration: '3분',
+    instructions: '대한민국 서울특별시 중구 신당동 366-144까지 도보',
+    travelMode: 'WALKING',
+  },
+];
 const RouteField = () => (
   <>
     <S.Wrapper>
@@ -36,7 +67,12 @@ const RouteField = () => (
         <S.Label>상세 경로</S.Label>
       </div>
     </S.Wrapper>
-    <S.DetailRoute />
+    <S.DetailRoute
+      duration="1시간 40분"
+      arrivalTime="오후 11시 40분"
+      departureTime="오후 2시 40분"
+      steps={steps}
+    />
   </>
 
 );
